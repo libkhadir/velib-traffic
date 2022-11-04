d3116bec71def9d28f1a471e66e5874b4dc62fa6
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2022-11-04 21:37:26 +0100

    Update db-import.sh

--- db-import.sh
+++ db-import.sh
@@ -1,5 +1,5 @@
 now=$(date +%F)
-for i in {1..7}; do 
+for i in {2..8}; do 
   currentdate=$(date -d "$now -$i days" +"%F")
   echo "importing data for $currentdate"
   curl -O https://velib.nocle.fr/dump/$currentdate-data.db -k; 
