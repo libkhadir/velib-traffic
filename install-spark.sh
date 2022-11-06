b65ffeebeb591df5c6fd54945f07ae6c6118ea1c
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2022-11-06 15:43:09 +0100

    Update install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -2,4 +2,3 @@ sudo apt-get install file -qq
 wget -q https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 file spark-3.3.1-bin-hadoop3.tgz
 tar xf spark-3.3.1-bin-hadoop3.tgz
-mkdir exports
