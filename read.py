ee150d4a1944dc34a23a6b0fdee0b663bcd00515
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2023-01-03 14:44:16 +0100

    Update read.py

--- read.py
+++ read.py
@@ -28,13 +28,4 @@ for i in range(2, 9):
     df = pd.concat((df, pd.DataFrame.from_records(fetches)))
 print('db import done')
 
-_f_d = velib.execute("""select * from status
-    left join statusConso on statusConso.id = status.idConso""")
-names = list(map(lambda x: x[0], _f_d.description))
-names[13] = 'DROP'
-df.columns = names
-df = df.iloc[:, :15]
-df = df.drop(['DROP'], axis=1)
-
-df.to_csv('velib.csv')
-del df
+df.to_csv('prepare.csv')
