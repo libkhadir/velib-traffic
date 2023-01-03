c2c5248f2c96dec65c1062f5e62d5ee95792aa94
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2023-01-03 14:46:25 +0100

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -1,3 +1,24 @@
+import findspark
+from pyspark.sql import SparkSession, SQLContext
+from pyspark import SparkContext
+import os
+import numpy
+import pandas as pd
+import sqlite3
+from pyspark.sql import functions
+import matplotlib.pyplot as plt
+import seaborn as sn
+from datetime import date, timedelta
+
+now = date.today()
+
+for i in range(2, 9):
+  currentDate = now - timedelta(days=i)
+  value = str(currentDate)
+  dbName = value + '-data.db'
+  print('loading db : ', dbName)
+  velib = sqlite3.connect(dbName)
+
 
 df.read_csv('prepare.csv')
 
