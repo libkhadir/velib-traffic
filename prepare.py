59451873e3e0670df94ac3c06e3f7cbaaf52b43a
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2023-01-03 15:45:49 +0100

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -23,11 +23,11 @@ for i in range(2, 9):
 df = pd.read_csv('prepare.csv')
 
 _f_d = velib.execute("""select * from status
-    left join statusConso on statusConso.id = status.idConso""")
+    left join statusConso on statusConso.id = status.idConso limit 1""")
 names = list(map(lambda x: x[0], _f_d.description))
 names[13] = 'DROP'
 df.columns = names
 df = df.iloc[:, :15]
 df = df.drop(['DROP'], axis=1)
 
-df.to_csv('velib.csv')
+df.to_csv('velib.csv', index=False)
