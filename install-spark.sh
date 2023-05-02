4737c5561133208f4b004fbe1ffe533989726240
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2023-05-02 13:42:36 +0200

    Update install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -2,3 +2,4 @@ sudo apt-get install file -qq
 wget -q https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 file spark-3.3.1-bin-hadoop3.tgz
 tar xf spark-3.3.1-bin-hadoop3.tgz
+wget https://github.com/xerial/sqlite-jdbc/releases/download/3.41.2.1/sqlite-jdbc-3.41.2.1.jar
